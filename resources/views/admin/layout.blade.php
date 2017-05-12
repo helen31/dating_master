d302db3314693ed8cc44dc21ef2a81b469716053
Alena <a30058422@gmail.com>
2017-05-12 14:04:35 +0300

    delete jquery-3.2.1.min.js

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -12,10 +12,9 @@
     <title>{{ trans('admin.dashboard') }}</title>
 
     <!-- Latest compiled and minified CSS -->
-    <!--<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">-->
 
-    <link rel="stylesheet" href="{{ url('/assets/js/bootstrap-3.3.7/bootstrap.min.css') }}"><!--gla new bootstrap 3.3.7-->
     <link rel="stylesheet" href="{{ url('/assets/css/font-awesome.css') }}">
 
     <link rel="stylesheet" href="{{ url('/assets/css/default-theme.css') }}">
@@ -110,12 +109,8 @@
     <!-- jQuery -->
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
     <!-- Latest compiled and minified JS -->
-    <!--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>-->
-
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
     <script src="{{ url('/assets/js/modernizr.min.js') }}"></script>
-    <script src="{{ url('/assets/js/jquery-3.2.1.min.js') }}"></script>
-    <script src="{{ url('/assets/js/bootstrap-3.3.7/bootstrap.min.js') }}"></script><!--gla new bootstrap 3.3.7-->
-
     <script src="{{ url('/assets/js/jquery-ui-datepicker-1.12.1/jquery-ui.min.js') }}"></script><!--gla datapicler jquery-ui-->
     <script src="{{ url('/assets/js/myJquery-ui-datepicker-1.12.1.js') }}"></script><!--gla datapicler jquery-ui-->
 
