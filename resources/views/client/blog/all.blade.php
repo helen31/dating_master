f374d0755b9b8d555fec990632c7cbbba2f9a8c3
grigshev <grigshev@gmail.com>
2017-05-19 15:20:58 +0300

    blog logic fixed

--- resources/views/client/blog/all.blade.php
+++ resources/views/client/blog/all.blade.php
@@ -1,13 +1,11 @@
 @extends('client.app')
 
 @section('content')
-
     <div class="container">
         <div class="row" style="margin-bottom: 50px; margin-top: 50px">
             <div class="col-md-9" id="blog">
                 <div class="col-md-10 col-md-offset-1">
                     @foreach($posts as $p)
-
                             <div class="title"><h2>{{ $p->title }}</h2></div>
                             <div class="image text-center"><img src="{{ url('/uploads/blog/'.$p->cover_image) }}" width="70%"></div>
                             <div class="body text-justify">{!! mb_substr($p->body, 0, 500, 'UTF-8') . '...' !!}</div>
@@ -16,7 +14,7 @@
                     @endforeach
                 </div>
                 <div class="paginate text-center">
-                    {{ $posts->links() }}
+                    {{ $posts->render() }}
                 </div>
             </div>
 
